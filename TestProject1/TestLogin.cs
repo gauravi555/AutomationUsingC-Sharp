e93c6317d4cd8e781a2683a588e7e7ded3958579
Gauravi Ghode <135152856+gauravi555@users.noreply.github.com>
2024-07-31 09:30:49 +0530

    Update TestLogin.cs
    Code is commented

--- TestProject1/TestLogin.cs
+++ TestProject1/TestLogin.cs
@@ -1,27 +1,27 @@
-﻿using NUnit.Framework.Internal;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// ﻿using NUnit.Framework.Internal;
+// using OpenQA.Selenium;
+// using OpenQA.Selenium.Chrome;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
 
-namespace TestProject1
-{
-    internal class TestLogin
-    {
-        [Test]
-        [SetUp]
-        public void Login()
-        {
-            IWebDriver driver;
-            driver = new ChromeDriver();
+// namespace TestProject1
+// {
+//     internal class TestLogin
+//     {
+//         [Test]
+//         [SetUp]
+//         public void Login()
+//         {
+//             IWebDriver driver;
+//             driver = new ChromeDriver();
 
-        }
+//         }
 
-        [Test]
-        public void 
+//         [Test]
+//         public void 
 
-    }
-}
+//     }
+// }
