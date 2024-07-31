1347cbf81c51dd730143267492e99ff4073189f5
Gauravi Ghode <135152856+gauravi555@users.noreply.github.com>
2024-07-31 09:38:19 +0530

    Update LoginPage.cs

--- TestProject1/Pages/LoginPage.cs
+++ TestProject1/Pages/LoginPage.cs
@@ -1,38 +1,38 @@
-﻿using OpenQA.Selenium;
-using SeleniumExtras.PageObjects;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// ﻿using OpenQA.Selenium;
+// using SeleniumExtras.PageObjects;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
 
-namespace TestProject1.Pages
-{
-    internal class LoginPage
-    {
-        private IWebDriver driver;
-        [FindsBy(How = How.Id, Using = "Input Text")]
-        private IWebElement emailtext;
-        [FindsBy(How = How.Id, Using = "Password")]
-        private IWebElement passwordtext;
-        [FindsBy(How = How.Id, Using = "LoginButton")]
-        private IWebElement Login;
+// namespace TestProject1.Pages
+// {
+//     internal class LoginPage
+//     {
+//         private IWebDriver driver;
+//         [FindsBy(How = How.Id, Using = "Input Text")]
+//         private IWebElement emailtext;
+//         [FindsBy(How = How.Id, Using = "Password")]
+//         private IWebElement passwordtext;
+//         [FindsBy(How = How.Id, Using = "LoginButton")]
+//         private IWebElement Login;
 
-        public LoginPage (IWebDriver driver)
-        {
-            this.driver = driver;
-            PageFactory.InitElements(driver, this);
+//         public LoginPage (IWebDriver driver)
+//         {
+//             this.driver = driver;
+//             PageFactory.InitElements(driver, this);
 
-        }
+//         }
 
-        public void login(string username, string password)
-        {
-            emailtext.SendKeys(username);
-            passwordtext.SendKeys(password);
-            Login.Click();
+//         public void login(string username, string password)
+//         {
+//             emailtext.SendKeys(username);
+//             passwordtext.SendKeys(password);
+//             Login.Click();
 
-        }
+//         }
 
 
-    }
-}
+//     }
+// }
